d28f0c4fcd5dd8f541245ba186dca4e2082c7b01
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-02-19 19:21:25 +0100

    Change name of module

--- pybind_torch/setup.py
+++ pybind_torch/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-setup(name='test_pybind_torch',
+setup(name='pybind_torch',
       ext_modules=[CppExtension('pybind_torch', ['pybind_torch.cpp'])],
       cmdclass={'build_ext': BuildExtension})
